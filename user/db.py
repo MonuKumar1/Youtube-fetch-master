e91c09c24ea977e28f26f977406109ee8401a28f
Monu Kumar <monu130601@gmail.com>
2021-12-04 15:47:31 +0530

    updating certification in db.py

--- user/db.py
+++ user/db.py
@@ -2,12 +2,12 @@ import pymongo
 from os import environ, path
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
-
+import certifi
 mongoURI = environ.get('MONGO_URI')
 
 def configDB():
     print('connecting to database...')
-    client = pymongo.MongoClient(mongoURI, serverSelectionTimeoutMS=30000)
+    client = pymongo.MongoClient(mongoURI, serverSelectionTimeoutMS=30000, tlsCAFile=certifi.where())
     database = client.get_database('YoutubeData')
     db = database.ysearch 
     print('Connected to Database!!')
